79d8671956ad7139cddd98f4a32b891507a41b33
atharv <atharv55215@gmail.com>
2026-02-07 01:14:44 +0530

    Adding support for methods get, post, put, delete, update

--- internal/execution/execute.go
+++ internal/execution/execute.go
@@ -14,26 +14,40 @@ func Execute(api configparser.ApiConfig, output string) TerminalOutput {
 	terminal_output := TerminalOutput{}
 	terminal_output.ApiName = api.Name
 	api_method := methods.Method(strings.ToUpper(api.Method))
+
+	req, err := http.NewRequest(string(api_method), api.Url, nil)
+	if err != nil {
+		terminal_output.Status = FAIL
+		terminal_output.TimeTaken = "0ms"
+		terminal_output.StatusCode = "-"
+		return terminal_output
+	}
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
 	start := time.Now()
-	res, err := http.NewRequest(string(api_method), api.Url, nil)
+	res, err := client.Do(req)
 	time_duration := time.Since(start)
 	durationStr := time_duration.Round(time.Millisecond).String()
+
 	if err != nil {
 		terminal_output.Status = FAIL
 		terminal_output.TimeTaken = durationStr
-		terminal_output.StatusCode = ""
+		terminal_output.StatusCode = "-"
 		return terminal_output
-	} else {
-		defer res.Body.Close()
-		terminal_output.StatusCode = strconv.Itoa(res.Response.StatusCode)
-		terminal_output.TimeTaken = durationStr
-
-		if res.Response.StatusCode >= 200 && res.Response.StatusCode < 300 {
-			terminal_output.Status = PASS
-		} else {
-			terminal_output.Status = FAIL
-		}
-
 	}
+
+	defer res.Body.Close()
+	terminal_output.StatusCode = strconv.Itoa(res.StatusCode)
+	terminal_output.TimeTaken = durationStr
+
+	if res.StatusCode >= 200 && res.StatusCode < 300 {
+		terminal_output.Status = PASS
+	} else {
+		terminal_output.Status = FAIL
+	}
+
 	return terminal_output
 }
